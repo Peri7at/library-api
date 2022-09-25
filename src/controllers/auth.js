583140870c7a28fc7aed41a0cbc225e9c00cc00b
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-09-25 16:27:54 +0300

    create controllers

---
+++ src/controllers/auth.js
@@ -0,0 +1,97 @@
+const jwt = require("jsonwebtoken");
+
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+
+const User = require("../models/user");
+
+module.exports = {
+  signInUser: async (req, res) => {
+    const { username, email, password, rememberMe } = req.body;
+    let cookieAge = 24 * 3600; // Default cookie expiry time is 1 day
+    try {
+      const user = await User.findOne({
+        $or: [{ email }, { username }],
+      });
+      if (!user) throw new Error("Wrong username or email");
+      const validPassword = await bcrypt.compare(password, user.password_hash);
+      if (!validPassword) throw new Error("Wrong password");
+
+      const payload = {
+        user: {
+          username: user.username,
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          _id: user._id,
+        },
+      };
+
+      if (rememberMe) {
+        cookieAge *= 14; // Remember me setting extends cookie expiry time to 2 weeks
+      }
+
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: cookieAge,
+        subject: user._id.toString(),
+      });
+
+      res.cookie("token", token, {
+        maxAge: cookieAge * 1000,
+        httpOnly: true,
+      });
+      res.json({ message: "Successfully signed in" });
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+  // eslint-disable-next-line consistent-return
+  signUpUser: async (req, res) => {
+    try {
+      const {
+        firstName,
+        lastName,
+        username,
+        email,
+        password,
+        passwordConfirm,
+        address,
+        acceptTerms,
+      } = req.body;
+
+      if (await User.exists({ username })) {
+        throw new Error("Username already used");
+      }
+      if (await User.exists({ email })) {
+        throw new Error("Email already used");
+      }
+      if (password !== passwordConfirm) {
+        throw new Error("Passwords do not match");
+      }
+
+      const passwordHash = await bcrypt.hash(password, saltRounds);
+      const newUser = await User.create({
+        firstName,
+        lastName,
+        username,
+        email,
+        password_hash: passwordHash,
+        acceptTerms,
+        address,
+      });
+      newUser.password_hash = undefined;
+      res.json(newUser);
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+  signOutUser: (req, res) => {
+    try {
+      res.clearCookie("token");
+      res.json({ success: true });
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+};
---
+++ src/controllers/book.js
@@ -0,0 +1,131 @@
+const { ObjectId } = require("mongoose").Types;
+const BookModel = require("../models/book");
+
+module.exports = {
+  getAllBooks: async (req, res) => {
+    try {
+      const books = await BookModel.find().populate("owner", {
+        password_hash: 0,
+      });
+      if (books.length <= 0) throw new Error("No books found");
+      res.json(books);
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+  getSingleBook: async (req, res) => {
+    try {
+      const BookId = req.params.id;
+      if (String(new ObjectId(BookId)) !== BookId.toString())
+        throw new Error("Requested book ID is not valid!");
+      const book = await BookModel.findById(BookId).populate("owner", {
+        password_hash: 0,
+      });
+      if (!book) throw new Error("There is no book with the provided ID!");
+      res.json(book);
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+  getAvailableBooks: async (req, res) => {
+    try {
+      const availableBooks = await BookModel.find({
+        isAvailable: true,
+      }).populate("owner", {
+        password_hash: 0,
+      });
+      if (availableBooks.length <= 0)
+        throw new Error("There are no available books at the moment!");
+      res.json(availableBooks);
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+  getFilteredBooks: async (req, res) => {
+    try {
+      const typeQuery = req.query.type;
+      if (!typeQuery) throw new Error("Type query parameter is required!");
+      const filteredBooks = await BookModel.find({
+        type: typeQuery,
+        isAvailable: true,
+      }).populate("owner", {
+        password_hash: 0,
+      });
+      if (filteredBooks.length <= 0)
+        throw new Error(`There are no available books of type ${typeQuery}!`);
+      res.json(filteredBooks);
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+  updateBook: async (req, res) => {
+    const { id } = req.params;
+    try {
+      // the following line is to make sure that the owner of the book does not get updated
+      req.body.owner = req.user._id;
+      const updatedBook = await BookModel.findByIdAndUpdate(
+        id,
+        { $set: req.body },
+        { new: true }
+      ).populate("owner", {
+        password_hash: 0,
+      });
+      if (!updatedBook) {
+        throw new Error("The book with the specified ID was not found.");
+      }
+      res.json(updatedBook);
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+  addBook: async (req, res) => {
+    try {
+      req.body.owner = req.user._id;
+      await BookModel.create(req.body);
+      res.status(201).json({ message: "Book created successfully." });
+    } catch (err) {
+      res.status(422).json({ message: err.message });
+    }
+  },
+  deleteBook: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const book = await BookModel.findByIdAndDelete(id);
+      if (!book) {
+        throw new Error("The book with the specified ID was not found.");
+      }
+      res.status(204).end();
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+
+  borrowBook: async (req, res) => {
+    try {
+      const { requestedBookId } = req.body;
+      const borrowerId = req.user._id;
+      if (String(new ObjectId(requestedBookId)) !== requestedBookId.toString())
+        throw new Error("Requested book ID is not valid!");
+
+      const book = await BookModel.findById(requestedBookId);
+
+      if (!book) {
+        throw new Error("The book with the specified ID was not found.");
+      }
+
+      if (!book.isAvailable)
+        throw new Error(
+          "The book with the specified ID is not available for donation."
+        );
+
+      book.count -= 1;
+      book.borrowers.push(borrowerId);
+      if (book.count === 0) book.isAvailable = false;
+
+      await book.save();
+      res.json({ message: "Book borrowed successfully" });
+    } catch (err) {
+      res.status(422).json({ message: err.message ?? err });
+    }
+  },
+};
